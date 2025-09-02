34615396214b0228bf0c0e6112cd5925971c7835
coderadi12345 <adityasharma74304@gmail.com>
2025-09-02 12:50:58 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      Hello
+    </>
+  )
+}
+
+export default App
