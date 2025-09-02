cd2b89cb5142ea3e7cf3cb450977eb23e31e346d
coderadi12345 <adityasharma74304@gmail.com>
2025-09-02 16:07:35 +0530

    Added basic backend

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+import express from "express"
+import cors from 'cors' 
+import {PORT} from './config/serverConfig.js'
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cors())
+
+app.get('/ping' , (req,res)=>{
+    return res.json({message: 'pong'})
+})
+app.listen(PORT,()=>{
+    console.log(`Server running at port ${PORT}`)
+})
